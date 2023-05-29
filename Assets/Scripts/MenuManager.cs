820b1fe633324302f6d2320f05d4ae9501ec14a4
John Pandis <ioanpand@outlook.com>
2023-05-29 04:28:52 +0300

    fade in screen added

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -1,33 +1,98 @@
 using System;
 using UnityEngine;
 using Cinemachine;
+using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour {
 
+    [Header("--- Start Game ---")]
     [SerializeField] private float delayGameStart = 1.85f;
+    [SerializeField] private GameObject crosshairCanvas;
     [SerializeField] CinemachineVirtualCamera mainVirtualCamera;
+    [SerializeField] CinemachineInputProvider inputProvider;
     [SerializeField] GameObject cameraUI;
     [SerializeField] AudioSource ambientRain;
     [SerializeField] private float ambientRainGameplayPitch = 0.75f;
+    [SerializeField] private float ambientRainMenuPitch = 2.5f;
     [SerializeField] AudioSource ambientEnv;
+
+    [Header("--- How To Play ---")]
+    [SerializeField] private RectTransform howToPlayUIObject;
+    [SerializeField] private float timeToFade;
+
+    [Header("--- Pause Game ---")]
+    [SerializeField] private GameObject pauseMenu;
+
+    private bool fadeHowToPlay = false;
+    private bool startGame = false;
+    private bool pauseGame = true;
 
     private void Awake() {
 
         Time.timeScale = 0;
     }
 
+    private void Update() {
+
+        FadeHowToPlay();
+
+        if (!startGame) {
+            return;
+        }
+
+        Cursor.visible = !pauseGame;
+
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+
+            pauseGame = !pauseGame;
+            ResumeGame();
+        }
+    }
+
     public void StartGame() {
 
         mainVirtualCamera.Priority = 10;
-        Invoke(nameof(DelayGameStart), delayGameStart);
+        Invoke(nameof(GameState), delayGameStart);
         Time.timeScale = 1;
-        
     }
 
-    private void DelayGameStart() {
+    public void HowToPlay() {
 
-        cameraUI.SetActive(true);
-        ambientRain.pitch = ambientRainGameplayPitch;
-        ambientEnv.Play();
+        fadeHowToPlay = !fadeHowToPlay;
+    }
+
+    public void ExitGame() {
+
+        Application.Quit();
+    }
+
+    public void ResumeGame() {
+
+        Time.timeScale = Convert.ToInt32(pauseGame);
+        inputProvider.enabled = pauseGame;
+        pauseMenu.SetActive(!pauseGame);
+    }
+
+    private void FadeHowToPlay() {
+
+        Vector3 currentScale = howToPlayUIObject.localScale;
+        Vector3 targetScale = fadeHowToPlay ? Vector3.one : Vector3.zero;
+
+        howToPlayUIObject.localScale = Vector3.Lerp(currentScale, targetScale, timeToFade * Time.unscaledDeltaTime);
+    }
+
+    private void GameState() {
+
+        startGame = !startGame;
+        cameraUI.SetActive(startGame);
+        inputProvider.enabled = startGame;
+        crosshairCanvas.SetActive(startGame);
+
+        float pitch = startGame ? ambientRainGameplayPitch : ambientRainMenuPitch;
+        ambientRain.pitch = pitch;
+
+        if (!ambientEnv.isPlaying) {
+            ambientEnv.Play();
+        }
     }
 }
---
+++ Assets/Scripts/UI/FadeScene.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FadeScene : MonoBehaviour {
+
+    [SerializeField] private CanvasGroup canvasGroup;
+    [SerializeField] private float transitionTime = 1;
+
+    private AnimationCurve curve = AnimationCurve.Linear(0, 0, 1, 1);
+    private float animationPoints = 0;
+    private bool fadeOut = false;
+
+    private void Awake() {
+
+        canvasGroup.alpha = 1;
+    }
+
+    private void Start() {
+        
+        ReloadScene(true);
+    }
+
+    private void Update() {
+
+        if (fadeOut) {
+
+            if (animationPoints <= 1)
+            animationPoints += transitionTime * Time.unscaledDeltaTime;
+            canvasGroup.alpha = Mathf.Lerp(1, 0, curve.Evaluate(animationPoints));
+
+            if (canvasGroup.alpha == 0) {
+
+                canvasGroup.interactable = false;
+                canvasGroup.blocksRaycasts = false;
+                //SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+        }
+        else {
+
+            animationPoints += transitionTime * Time.unscaledDeltaTime;
+            canvasGroup.alpha = Mathf.Lerp(0, 1, curve.Evaluate(animationPoints));
+
+            
+
+            if (canvasGroup.alpha == 1) {
+
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+        }
+    }
+    
+    public void ReloadScene(bool fade) {
+
+        animationPoints = 0;
+        fadeOut = fade;
+
+        if (!fadeOut) {
+
+            canvasGroup.interactable = true;
+            canvasGroup.blocksRaycasts = true;
+        }
+    }
+}
