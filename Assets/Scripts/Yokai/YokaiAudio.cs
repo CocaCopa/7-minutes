e8cd89153d6b3f1297dca18ea9593f41734196a4
John Pandis <ioanpand@outlook.com>
2023-05-29 07:05:15 +0300

    added sounds / minor ui adjustments

--- Assets/Scripts/Yokai/YokaiAudio.cs
+++ Assets/Scripts/Yokai/YokaiAudio.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class YokaiAudio : MonoBehaviour {
 
     [SerializeField] private AudioClip doorJumpscare;
+    [SerializeField] private AudioClip getTheKeySFX;
+    [SerializeField] private float delayTime;
+    [SerializeField] private AudioClip laughterSFX;
 
     private AudioSource audioSource;
 
@@ -16,6 +19,23 @@ public class YokaiAudio : MonoBehaviour {
     private void Start() {
 
         YokaiObserver.Instance.OnDoorOpenJumpscare += Observer_OnDoorOpenJumpscare;
+        YokaiObserver.Instance.OnBasementEventJumpscare += Observer_OnBasementEventJumpscare;
+        YokaiObserver.Instance.OnBasementEventComplete += Observer_OnBasementEventComplete;
+    }
+
+    private void Observer_OnBasementEventComplete(object sender, System.EventArgs e) {
+
+        audioSource.PlayOneShot(laughterSFX, 1.35f);
+    }
+
+    private void Observer_OnBasementEventJumpscare(object sender, System.EventArgs e) {
+
+        Invoke(nameof(DelayAudioClipGetTheKey), delayTime);
+    }
+
+    private void DelayAudioClipGetTheKey() {
+
+        audioSource.PlayOneShot(getTheKeySFX, 1.5f);
     }
 
     private void Observer_OnDoorOpenJumpscare(object sender, System.EventArgs e) {
